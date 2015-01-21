2c84f3ebd1eb2c0c5ade99d3cfccaf7db8be32e3
dyno2097 <dillonroberts007@gmail.com>
2015-01-21 08:20:49 -0600

    none

--- 1_4_7 4.py
+++ 1_4_7 4.py
@@ -3,20 +3,20 @@ import matplotlib.pyplot as plt
 import os.path              
 
 directory = os.path.dirname(os.path.abspath(__file__))  
-student_file = os.path.join(directory, 'storm.jpg')
+student_file = os.path.join(directory, 'lightning.png')
 
-storm_img = PIL.Image.open(student_file)
+lightning_img = PIL.Image.open(student_file)
 fig, axes = plt.subplots(1, 2)
-axes[0].imshow(storm_img, interpolation='none')
+axes[0].imshow(lightning_img, interpolation='none')
 
-axes[1].imshow(storm_img, interpolation='none')
+axes[1].imshow(lightning_img, interpolation='none')
 axes[1].set_xticks(range(1050, 1410, 100))
 axes[1].set_xlim(1050, 1400) 
 axes[1].set_ylim(1100, 850)
 fig.show()
 
 
-water_file = os.path.join(directory, 'water.jpg')
+water_file = os.path.join(directory, 'water.png')
 water_img = PIL.Image.open(water_file)
 water = water_img.resize((89, 87)) 
 fig2, axes2 = plt.subplots(1, 2)
@@ -24,11 +24,11 @@ axes2[0].imshow(water_img)
 axes2[1].imshow(water)
 fig2.show()
 
-storm_img.paste(water, (730, 950), mask=water) 
+lightning_img.paste(water, (730, 950), mask=water) 
 
 fig3, axes3 = plt.subplots(1, 2)
-axes3[0].imshow(storm_img, interpolation='none')
-axes3[1].imshow(storm_img, interpolation='none')
+axes3[0].imshow(lightning_img, interpolation='none')
+axes3[1].imshow(lightning_img, interpolation='none')
 axes3[1].set_xlim(500, 1500)
 axes3[1].set_ylim(1130, 850)
 fig3.show()
